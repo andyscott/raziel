ff8ad9fc8583896c5390ff3c720b2cd2fbc20191
Andy Scott <andy.g.scott@gmail.com>
2019-03-18 23:12:38 -0700

    Nuke dummy stats app

--- src/Bazel/BuildEventStream/StatsApp.hs
+++
@@ -1,7 +0,0 @@
-module Bazel.BuildEventStream.StatsApp
-  ( main
-  )
-  where
-
-main :: IO ()
-main = putStrLn "Hello World!"
--- src/Raziel/Main.hs
+++ src/Raziel/Main.hs
@@ -13,6 +13,7 @@ import Bazel.Label
 data Command
   = Lint
   | Move Label Label
+  | Stats
 
 commandMain :: Command -> IO ()
 
@@ -28,8 +29,12 @@ main = commandMain =<< execParser opts
 
     commandOpts :: Parser Command
     commandOpts = hsubparser
-      (  command "lint" (info lintOpts ( progDesc "lint a build" ))
-      <> command "move" (info moveOpts ( progDesc "refactor/move labels" ))
+      (  command "lint"  ( info lintOpts
+                         ( progDesc "lint a build" ))
+      <> command "move"  ( info moveOpts
+                         ( progDesc "refactor/move labels" ))
+      <> command "stats" ( info statsOpts
+                         ( progDesc "calculate stats from the build event stream" ))
       <> commandGroup "WIP commands:"
       )
 
@@ -40,6 +45,9 @@ main = commandMain =<< execParser opts
     moveOpts = Move
       <$> argument lbl (metavar "<label-from>")
       <*> argument lbl (metavar "<label-to>")
+
+    statsOpts :: Parser Command
+    statsOpts = pure $ Stats
 
     lbl :: ReadM Label
     lbl = maybeReader $ parseLabel . pack
