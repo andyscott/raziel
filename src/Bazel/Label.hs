1aaa15255e84812024d22ce090ea063402a4ee59
Andy Scott <andy.g.scott@gmail.com>
2019-03-24 22:44:28 -0700

    Stronger invariants for Label

--- src/Bazel/Label.hs
+++ src/Bazel/Label.hs
@@ -4,8 +4,9 @@
 {-# LANGUAGE DeriveLift #-}
 
 module Bazel.Label
-  ( Label (..)
+  ( Label
   , parseLabel
+  , maybeLabel
   , parseLabelQ
   , qlabel
   )
@@ -13,7 +14,7 @@ module Bazel.Label
 
 import Prelude hiding (concat)
 
-import           Control.Applicative
+import           Control.Applicative hiding (many)
 import           Control.Exception
 import           Control.Monad.Catch
 import           Data.Bifunctor
@@ -29,45 +30,126 @@ import           Language.Haskell.TH
 import           Language.Haskell.TH.Syntax
 import           Language.Haskell.TH.Quote
 
-data Label
-  = Label
-  { labelRepositoryName :: Maybe Text
-  , labelPackageName :: Maybe Text
-  , labelName :: Maybe Text
-  }
+newtype Label = Label RawLabel
   deriving (Lift, Eq, Ord)
 
-{-|
-resolvedLabelName :: Label -> Text
-resolvedLabelName l =
-  fromMaybe nameFromPackageName $ labelName l
-  where
-    nameFromPackageName :: String
-    nameFromPackageName =
-      (pack . reverse . takeWhile (/='/') . reverse . unpack $ labelPackageName l)
--}
-
-instance Hashable Label where
-  hashWithSalt s (Label a b c) =
-    s `hashWithSalt`
-    a `hashWithSalt`
-    b `hashWithSalt` c
-
 instance Show Label where
-  show (Label Nothing Nothing Nothing) = "???"
-  show (Label (Just r) Nothing Nothing) = unpack $ "@" <> r
-  show (Label Nothing Nothing (Just n)) = unpack $ ":" <> n
-  show (Label r p n) = unpack $
-    (maybe "" (\r' -> "@" <> r') r) <>
-    "//" <> (fromMaybe "" p) <> (maybe "" (\n' -> ":" <> n') n)
+  show l @ (Label rl) = case rl of
+    RawLabelRPN r p n -> unpack $ "@" <> r <> "//" <> p <> ":" <> n
+    RawLabelR   r     -> unpack $ "@" <> r
+    RawLabelP     p   -> unpack $             "//" <> p
+    RawLabelN       n -> unpack $                          ":" <> n
+    RawLabelRP  r p   -> unpack $ "@" <> r <> "//" <> p
+    RawLabelRN  r   n -> unpack $ "@" <> r <> "//" <>      ":" <> n
+    RawLabelPN    p n -> unpack $             "//" <> p <> ":" <> n
 
 data LabelException
   = UnableToParseLabel (ParseErrorBundle Text Void)
+  | EmptyLabelException Text
   deriving (Show, Typeable)
 
 instance Exception LabelException
 
+labelRepositoryName :: Label -> Maybe Text
+labelRepositoryName (Label rl) = rawLabelRepositoryName rl
+
+labelPacakgeName :: Label -> Maybe Text
+labelPacakgeName (Label rl) = rawLabelPackageName rl
+
+labelName :: Label -> Text
+labelName (Label rl) = case rl of
+  RawLabelRPN _ _ n -> n
+  RawLabelR   r     -> r
+  RawLabelP     p   -> lastPackageComponent p
+  RawLabelN       n -> n
+  RawLabelRP  _ p   -> lastPackageComponent p
+  RawLabelRN  _   n -> n
+  RawLabelPN    _ n -> n
+
+reifyName :: Label -> Label
+reifyName l @ (Label rl) = case rl of
+  RawLabelRPN _ _ n -> l
+  RawLabelR   r     -> Label $ RawLabelRN r $ r
+  RawLabelP     p   -> Label $ RawLabelPN p $ lastPackageComponent p
+  RawLabelN       n -> l
+  RawLabelRP  r p   -> Label $ RawLabelRPN r p $ lastPackageComponent p
+  RawLabelRN  _   n -> l
+  RawLabelPN    _ n -> l
+
+data RawLabel
+  = RawLabelRPN Text Text Text
+  | RawLabelR   Text
+  | RawLabelP        Text
+  | RawLabelN             Text
+  | RawLabelRP  Text Text
+  | RawLabelRN  Text      Text
+  | RawLabelPN       Text Text
+  deriving (Lift, Eq, Ord)
+
+rawLabelRepositoryName :: RawLabel -> Maybe Text
+rawLabelRepositoryName rl = case rl of
+  RawLabelRPN r _ _ -> Just r
+  RawLabelR   r     -> Just r
+  RawLabelP     _   -> Nothing
+  RawLabelN       _ -> Nothing
+  RawLabelRP  r _   -> Just r
+  RawLabelRN  r _   -> Just r
+  RawLabelPN    _ _ -> Nothing
+
+rawLabelPackageName :: RawLabel -> Maybe Text
+rawLabelPackageName rl = case rl of
+  RawLabelRPN _ p _ -> Just p
+  RawLabelR   _     -> Nothing
+  RawLabelP     p   -> Just p
+  RawLabelN       _ -> Nothing
+  RawLabelRP  _ p   -> Just p
+  RawLabelRN  _   _ -> Nothing
+  RawLabelPN    p _ -> Just p
+
+rawLabelName :: RawLabel -> Maybe Text
+rawLabelName rl = case rl of
+  RawLabelRPN _ _ n -> Just n
+  RawLabelR   _     -> Nothing
+  RawLabelP     _   -> Nothing
+  RawLabelN       n -> Just n
+  RawLabelRP  _ _   -> Nothing
+  RawLabelRN  _   n -> Just n
+  RawLabelPN    _ n -> Just n
+
+
+packageComponents :: Text -> [Text]
+packageComponents t = case runParser p "string" t of
+  Left e   -> [t]
+  Right cs -> cs
+  where
+    p :: Parser [Text]
+    p =
+      (:)
+      <$> component
+      <*> many (char '/' >> component)
+    component :: Parser Text
+    component = takeWhile1P Nothing (/= '/')
+
+lastPackageComponent :: Text -> Text
+lastPackageComponent = last . packageComponents
+
 type Parser = Parsec Void Text
+
+maybeLabel :: Maybe Text -> Maybe Text -> Maybe Text -> Maybe Label
+maybeLabel r p n = Label <$> maybeRawLabel r p n
+
+maybeRawLabel :: Maybe Text -> Maybe Text -> Maybe Text -> Maybe RawLabel
+maybeRawLabel (Just r) (Just p) (Just n) = Just $ RawLabelRPN r p n
+maybeRawLabel (Just r)  Nothing  Nothing = Just $ RawLabelR   r
+maybeRawLabel  Nothing (Just p)  Nothing = Just $ RawLabelP     p
+maybeRawLabel  Nothing  Nothing (Just n) = Just $ RawLabelN       n
+maybeRawLabel (Just r) (Just p)  Nothing = Just $ RawLabelRP  r p
+maybeRawLabel (Just r)  Nothing (Just n) = Just $ RawLabelRN  r   n
+maybeRawLabel  Nothing (Just p) (Just n) = Just $ RawLabelPN    p n
+maybeRawLabel  Nothing  Nothing  Nothing = Nothing
+
+parseLabel :: Text -> Either SomeException Label
+parseLabel l = Label <$> parseRawLabel l
 
 parseLabelQ :: String -> Q Exp
 parseLabelQ t =
@@ -85,13 +167,14 @@ qlabel
   where notHandled things = error $
           things ++ " are not handled by the label quasiquoter."
 
-parseLabel :: Text -> Either SomeException Label
-parseLabel l = case runParser p "string" l of
-  Left e  -> throwM $ UnableToParseLabel e
-  Right v -> Right v
+parseRawLabel :: Text -> Either SomeException RawLabel
+parseRawLabel l = case runParser p "string" l of
+  Left e          -> throwM $ UnableToParseLabel e
+  Right (Just v)  -> Right v
+  Right (Nothing) -> throwM $ EmptyLabelException l
   where
-    p :: Parser Label
-    p = Label <$> repo <*> package <*> name
+    p :: Parser (Maybe RawLabel)
+    p = maybeRawLabel <$> repo <*> package <*> name
 
     -- see:
     -- https://github.com/bazelbuild/bazel/blob/0.23.1/
--- src/Bazel/LabelSpec.hs
+++ src/Bazel/LabelSpec.hs
@@ -16,29 +16,28 @@ unitTests = testGroup "Label" [showTests, parseLabelTests]
 
 showTests :: TestTree
 showTests = testGroup "show"
-  [ check (Label  Nothing       Nothing          Nothing)     "???"
-  , check (Label  Nothing      (Just "foo/bar")  Nothing)     "//foo/bar"
-  , check (Label  Nothing       Nothing         (Just "baz")) ":baz"
-  , check (Label  Nothing      (Just "foo/bar") (Just "baz")) "//foo/bar:baz"
-  , check (Label (Just "zedd")  Nothing          Nothing)     "@zedd"
-  , check (Label (Just "zedd") (Just "foo/bar")  Nothing)     "@zedd//foo/bar"
-  , check (Label (Just "zedd")  Nothing         (Just "baz")) "@zedd//:baz"
-  , check (Label (Just "zedd") (Just "foo/bar") (Just "baz")) "@zedd//foo/bar:baz"
+  [ check (label  Nothing      (Just "foo/bar")  Nothing)     "//foo/bar"
+  , check (label  Nothing       Nothing         (Just "baz")) ":baz"
+  , check (label  Nothing      (Just "foo/bar") (Just "baz")) "//foo/bar:baz"
+  , check (label (Just "zedd")  Nothing          Nothing)     "@zedd"
+  , check (label (Just "zedd") (Just "foo/bar")  Nothing)     "@zedd//foo/bar"
+  , check (label (Just "zedd")  Nothing         (Just "baz")) "@zedd//:baz"
+  , check (label (Just "zedd") (Just "foo/bar") (Just "baz")) "@zedd//foo/bar:baz"
   ]
   where
     check :: Label -> String -> TestTree
-    check label expected = testCase ("show " ++ expected) $ (show label) @?= expected
+    check l expected = testCase ("show " ++ expected) $ (show l) @?= expected
 
 parseLabelTests :: TestTree
 parseLabelTests = testGroup "parseLabel" [yays, nays]
   where
     yays = testGroup "valid"
-      [ yay "//foo/bar" $ Label Nothing (Just "foo/bar") Nothing
-      , yay "//foo/bar:baz" $ Label Nothing (Just "foo/bar") (Just "baz")
-      , yay "@zip//foo/bar" $ Label (Just "zip") (Just "foo/bar") Nothing
-      , yay "@zip//foo/bar:zap" $ Label (Just "zip") (Just "foo/bar") (Just "zap")
-      , yay "@shellcheck//:bin/shellcheck" $ Label (Just "shellcheck") Nothing (Just "bin/shellcheck")
-      , yay "@io_company_x//src/scala/com/zedd:util" $ Label (Just "io_company_x") (Just "src/scala/com/zedd") (Just "util")
+      [ yay "//foo/bar" $ label Nothing (Just "foo/bar") Nothing
+      , yay "//foo/bar:baz" $ label Nothing (Just "foo/bar") (Just "baz")
+      , yay "@zip//foo/bar" $ label (Just "zip") (Just "foo/bar") Nothing
+      , yay "@zip//foo/bar:zap" $ label (Just "zip") (Just "foo/bar") (Just "zap")
+      , yay "@shellcheck//:bin/shellcheck" $ label (Just "shellcheck") Nothing (Just "bin/shellcheck")
+      , yay "@io_company_x//src/scala/com/zedd:util" $ label (Just "io_company_x") (Just "src/scala/com/zedd") (Just "util")
       ]
 
     nays = testGroup "invalid"
@@ -53,3 +52,8 @@ parseLabelTests = testGroup "parseLabel" [yays, nays]
 
     yay raw = check raw . Just
     nay raw = check raw Nothing
+
+label :: Maybe Text -> Maybe Text -> Maybe Text -> Label
+label r p n = case maybeLabel r p n of
+  Just l  -> l
+  Nothing -> undefined
